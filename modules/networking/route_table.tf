276ca607fb18bb11def95eaddb6dad9b7b1d2634
Low Chee Meng <lowcheemeng02@gmail.com>
2023-10-12 00:43:40 +0800

    add igw and route to igw from public subnets

--- modules/networking/route_table.tf
+++ modules/networking/route_table.tf
@@ -7,3 +7,9 @@ resource "aws_route_table_association" "public_subnet" {
   subnet_id      = element(aws_subnet.subnet.*.id, count.index)
   route_table_id = aws_route_table.rt_public.id
 }
+
+resource "aws_route" "route_to_igw" {
+  route_table_id = aws_route_table.rt_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
