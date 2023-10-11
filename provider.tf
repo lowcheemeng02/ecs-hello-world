2301337c80a3d74e2fa3c3ae8ac3667cd6a0f907
Low Chee Meng <lowcheemeng02@gmail.com>
2023-10-11 13:21:02 +0800

    use s3 as backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "sctp-ce3-tfstate-bucket-1"
+    key    = "terraform-ecs-hello-world.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
