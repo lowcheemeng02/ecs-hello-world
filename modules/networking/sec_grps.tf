bdc5ffbdd7764a20c2b68823174a9a425507a092
Low Chee Meng <lowcheemeng02@gmail.com>
2023-10-12 09:37:00 +0800

    add sec grp

---
+++ modules/networking/sec_grps.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "ecs_secgrp" {
+
+    name = "${var.proj_name}-secgrp"
+
+    vpc_id = aws_vpc.main.id
+    
+}
+
+# security group rules are "allow" rules
+# including an ip into the rule means allowing the traffic from that ip
+# specifying a port range in the rule means allowing the traffic within that port range
+resource "aws_security_group_rule" "ingress_rule" {
+  type              = "ingress"
+
+  # this rule applies to traffic within the port range: "from_port" to "to_port"
+  from_port         = 80
+  to_port           = 8080
+  
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # the ip range that this rule will allow
+  security_group_id = aws_security_group.ecs_secgrp.id
+}
+
+# security group rules are "allow" rules
+# including an ip into the rule means allowing the traffic from that ip
+# specifying a port range in the rule means allowing the traffic within that port range
+resource "aws_security_group_rule" "egress_rule" {
+  type              = "egress"
+
+  # this rule applies to traffic within the port range: "from_port" to "to_port"
+  from_port         = 80
+  to_port           = 8080
+  
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # the ip range that this rule will allow
+  security_group_id = aws_security_group.ecs_secgrp.id
+}
--- modules/networking/subnets.tf
+++ modules/networking/subnets.tf
@@ -4,5 +4,9 @@ resource "aws_subnet" "subnet" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index) # 30.0.X.0/24 mask for each subnet if vpc cidr block prefix is /16
   map_public_ip_on_launch = true
   availability_zone       = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.proj_name}-SN-public-${element(var.availability_zones, count.index)}"
+  }
 }
 
